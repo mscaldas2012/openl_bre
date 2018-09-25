938ead16d5856763708282bbc03ae703ee03040f
mcq1 <mcq1@cdc.gov>
2018-09-25 16:26:24 -0400

    fixing tests

--- src/main/java/com/baeldung/openltablets/rules/IRule.java
+++ src/main/java/com/baeldung/openltablets/rules/IRule.java
@@ -1,7 +1,7 @@
 package com.baeldung.openltablets.rules;
 
 public interface IRule {
-    //void helloUser(final Case aCase, final Response response);
-    Boolean validateCatchement(String state, String county);
+    Boolean validateCatchment(String state, String county, Integer age);
+
 
 }
--- src/main/java/com/baeldung/openltablets/rules/Main.java
+++ src/main/java/com/baeldung/openltablets/rules/Main.java
@@ -10,18 +10,26 @@ public class Main {
     public static void main(String[] args) {
         Main rules = new Main();
         final EngineFactory<IRule> engineFactory = new RulesEngineFactory<IRule>(Main.class.getClassLoader()
-                .getResource("openltablets/catchment2.xlsx"), IRule.class);
+                .getResource("openltablets/catchment.xlsx"), IRule.class);
         instance = engineFactory.newEngineInstance();
 
         rules.process("California", "San Francisco");
         rules.process("California", "Alameda");
-        rules.process("Georgia", "");
+        rules.process("Georgia", "", 17);
+        rules.process("Georgia", "", 29);
         rules.process("California", "Los Angeles");
         rules.process("Alaska", "Juneau");
     }
 
+    public void process(String state, String county, Integer age) {
+        long startTime = System.currentTimeMillis();
+        Boolean response = instance.validateCatchment(state, county, age);
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println(String.format("Processing(%s, %s, %d)= %s in %d ms.", state, county, age, response, totalTime));
+
+    }
     public void process(String state, String county) {
-        Boolean response = instance.validateCatchement(state, county);
-        System.out.println("response = " + response);
+        process(state, county, null);
     }
 }
